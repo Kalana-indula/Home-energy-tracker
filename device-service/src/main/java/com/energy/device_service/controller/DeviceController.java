f9e65289308da787ba0c53754d727d3710fd846c
Kalana-indula <kalanasmedia@outlook.com>
2026-01-11 14:21:07 +0530

    device update method added

--- device-service/src/main/java/com/energy/device_service/controller/DeviceController.java
+++ device-service/src/main/java/com/energy/device_service/controller/DeviceController.java
@@ -1,5 +1,7 @@
 package com.energy.device_service.controller;
 
+import com.energy.device_service.dto.DeviceDto;
+import com.energy.device_service.dto.response.SingleEntityResponse;
 import com.energy.device_service.entity.Device;
 import com.energy.device_service.service.DeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,7 @@ public class DeviceController {
         this.deviceService = deviceService;
     }
 
+//    Create device
     @PostMapping
     public ResponseEntity<?> createDevice(@RequestBody Device device){
         try{
@@ -26,6 +29,22 @@ public class DeviceController {
         }catch (Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
+    }
+
+//    get device by id
+    @GetMapping("{id}")
+    public ResponseEntity<?> getDeviceById(@PathVariable Long id){
+        SingleEntityResponse<DeviceDto> response=deviceService.getDeviceById(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    // update device
+    @PutMapping("{id}")
+    public ResponseEntity<?> updateDevice(@PathVariable Long id, @RequestBody Device device){
+        SingleEntityResponse<DeviceDto> response=deviceService.updateDevice(id, device);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
 //    methods
